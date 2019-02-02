d199e872b607591c17a0b4f1500d9df19f9bcc6e
stark-zhang <2623825899@qq.com>
2019-02-02 18:36:52 +0800

    test upload
    Signed-off-by: stark-zhang <2623825899@qq.com>

--- bwt/bwt.h
+++ bwt/bwt.h
@@ -6,6 +6,22 @@
 #ifndef __BWT_H
 #define __BWT_H
 
+#include <cstdint>
+
+namespace NXZIP
+{
+	class BWT_CoreClass
+	{
+	public:
+
+	private:
+		uint32_t index;
+		uint8_t* cstr;
+	};
+	
+	bool NXZ_BWTransform();
+}
+
 #endif /*__BWT_H*/
 
 //EOF
