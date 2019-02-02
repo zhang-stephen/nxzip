da13ad837570312a1b12b66b8a8e4e8af4329472
stark-zhang <2623825899@qq.com>
2019-02-02 16:55:03 +0800

    update ignore list

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/**
+ * The Main Cpp Source File in NXZIP
+ * 2019-02-02
+ */
+
+#include <iostream>
+#include <cstdlib>
+
+int main(int argc, char* argv[])
+{
+	return 0;
+}
+
+//EOF
---
+++ nxzip.h
@@ -0,0 +1,11 @@
+/**
+ * The Global Header file for main.cpp
+ * 2019-02-02
+**/
+
+#ifndef __NXZIP_H
+#define __NXZIP_H
+
+#endif /*__NXZIP*/
+
+//EOF
